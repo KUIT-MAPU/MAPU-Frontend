de9eece591f1618491c273c5dca35c9a937642ec
Demian <sykc2002@gmail.com>
2024-07-05 19:47:14 +0900

    feat:svg files added

--- src/components/userProfile/SideBar.tsx
+++ src/components/userProfile/SideBar.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './SideBar.module.scss';
+import {ReactComponent as Logo} from '../../assets/MapuLogo.svg';
 
 const SideBar = (props: { children: React.ReactNode }) => {
     return (
         <div className={styles.container}>
             <div className={styles.LeftSideBar}>
                 <Link to="/" className={styles.link}>
+                    <div className={`${styles.iconContainer} ${styles.logoIconContainer}`}>
+                        <Logo className={styles.icon} />
+                    </div>
+                </Link>
+                <Link to="/" className={styles.link}>
                     <div className={styles.iconContainer}>
                         <i className={`fas fa-home ${styles.icon}`}></i>
                     </div>
                 </Link>
-                <Link to="/timeline" className={styles.link}>
-                    <div className={styles.iconContainer}>
-                        <i className={`fas fa-info-circle ${styles.icon}`}></i>
-                    </div>
-                </Link>
                 <Link to="/explore" className={styles.link}>
                     <div className={styles.iconContainer}>
-                        <i className={`fas fa-cog ${styles.icon}`}></i>
+                        <i className={`fas fa-explore ${styles.icon}`}></i>
                     </div>
                 </Link>
                 <Link to="/user" className={styles.link}>
                     <div className={styles.iconContainer}>
-                        <i className={`fas fa-envelope ${styles.icon}`}></i>
+                        <i className={`fas fa-user ${styles.icon}`}></i>
                     </div>
                 </Link>
             </div>
