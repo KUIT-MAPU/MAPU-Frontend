21d684a1321c1f9be233f16096ccbca19bf0bddb
Demian <sykc2002@gmail.com>
2024-07-05 19:50:55 +0900

    feat:svg.d.ts file added

--- svg.d.ts
+++ svg.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="react-scripts" />
+<<<<<<< HEAD
 declare module '*.svg' {
     const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default value;
   }
+=======
+>>>>>>> aa93db65 (feat:svg.d.ts file added)
