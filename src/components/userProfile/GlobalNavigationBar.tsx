99caecccfc2714d971d44021b6061e2b954b6c2d
Demian <sykc2002@gmail.com>
2024-07-07 11:42:52 +0900

    feat:sidebar completed

--- src/components/userProfile/GlobalNavigationBar.tsx
+++ src/components/userProfile/GlobalNavigationBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import styles from './GlobalNavigationBar.module.scss';
 import {ReactComponent as MapuLogo} from '../../assets/MapuLogo.svg';
@@ -6,12 +6,15 @@ import {ReactComponent as Home} from '../../assets/Home.svg';
 import {ReactComponent as Explore} from '../../assets/Explore.svg';
 import {ReactComponent as User} from '../../assets/User.svg';
 import {ReactComponent as Logout } from '../../assets/Logout.svg';
+import {ReactComponent as Login } from '../../assets/Login.svg';
 
 const SideBar = (props: { children: React.ReactNode }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <div className={styles.container}>
             <div className={styles.LeftSideBar}>
-                <div className={`${styles.iconContainer} ${styles.logoIconContainer}`}>
+                <div className={styles.logoIconContainer}>
                     <MapuLogo className={styles.icon} />
                 </div>
                 <Link to="/" className={styles.link}>
