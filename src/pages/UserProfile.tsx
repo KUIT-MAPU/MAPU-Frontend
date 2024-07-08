c583b6aba6408537f21ca3b0a8f8fe18995bc96e
ro-el <chung3286@naver.com>
2024-07-08 21:46:39 +0900

    feat(UserProfile): 유저 페이지 path variable 추가

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import SideBar from '../components/userProfile/GlobalNavigationBar';
 import styles from '../components/userProfile/GlobalNavigationBar.module.scss';
 
 const UserProfile = () => {
+  const { userId } = useParams();
+
   useEffect(() => {
     const titleElement = document.getElementsByTagName('title')[0];
-    titleElement.innerHTML = `@@@님의 페이지 | MAPU`; //props로 받아와 넣는 것으로 수정
+    titleElement.innerHTML = `@@@님의 페이지 | MAPU`; //api 호출 -> 사용자 타이틀에 추가
   }, []);
 
   return (
