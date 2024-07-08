632ea22ac779d9ccf47dd8c364fb831877a2a60b
ro-el <chung3286@naver.com>
2024-07-08 21:35:30 +0900

    feat(UserProfile): 유저 페이지 타이틀 변경

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import SideBar from '../components/userProfile/GlobalNavigationBar'
+import SideBar from '../components/userProfile/GlobalNavigationBar';
 import styles from '../components/userProfile/GlobalNavigationBar.module.scss';
 
 const UserProfile = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `@@@님의 페이지 | MAPU`; //props로 받아와 넣는 것으로 수정
+  }, []);
+
   return (
     <div className={styles.container}>
-      <SideBar>
-        UserProfile
-      </SideBar>
+      <SideBar>UserProfile</SideBar>
       <main className={styles.main}>
         <Outlet />
       </main>
     </div>
   );
-}
+};
 
-export default UserProfile
+export default UserProfile;
