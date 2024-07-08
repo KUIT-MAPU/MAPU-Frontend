7fcb22c49e7f27ea8849c4818e21d26a25658401
ro-el <chung3286@naver.com>
2024-07-08 21:46:56 +0900

    feat(Map): 지도 페이지 path variable 추가

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Map = () => {
+  const { mapId } = useParams();
+
   useEffect(() => {
     const titleElement = document.getElementsByTagName('title')[0];
-    titleElement.innerHTML = `지도 | MAPU`;
+    titleElement.innerHTML = `지도명 | MAPU`; //api 호출 -> '지도명 | MAPU' 설정
   }, []);
 
   return <div>Map</div>;
