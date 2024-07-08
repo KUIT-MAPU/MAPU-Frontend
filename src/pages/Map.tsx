0e5a6fd147ad6969f50ae4c16bb4d5746fce00f0
ro-el <chung3286@naver.com>
2024-07-08 21:35:07 +0900

    feat(Map): 지도 페이지 타이틀 변경

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 const Map = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `지도 | MAPU`;
+  }, []);
+
   return <div>Map</div>;
 };
 
