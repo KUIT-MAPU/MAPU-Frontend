852a634e0cbfcf5bb20a1b11a59fc8e8664390ed
ro-el <chung3286@naver.com>
2024-07-10 20:11:45 +0900

    feat(LoginModal): 로그인 모달 생성 및 소셜로그인 버튼 구현
    현재 페이지가 랜딩페이지인 경우, 윗 부분(로그인 문구) 보여주지 않음

--- src/components/login/LoginModal.tsx
+++ src/components/login/LoginModal.tsx
@@ -1,5 +1,55 @@
-const LoginModal = () => {
-  return <div>LoginModal</div>;
+import { useLocation, useNavigate } from 'react-router-dom';
+import styles from './LoginModal.module.scss';
+
+import GreenTransparentLogo from '../../assets/mapu-logo/green-transparent.svg';
+import KakaoLogo from '../../assets/login/kakao.svg';
+import GoogleLogo from '../../assets/login/google.svg';
+
+interface Props {
+  isRegistering: boolean;
+  setIsRegistering: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoginModal: React.FC<Props> = ({ isRegistering, setIsRegistering }) => {
+  const navigate = useNavigate();
+  const pathname = useLocation().pathname;
+
+  return (
+    <div>
+      {`${pathname}` !== '/' && <div>hh</div>}
+      <div className={styles.btnContainer}>
+        <button
+          type="button"
+          className={`${styles.loginBtn} ${styles.kakaoBtn}`}
+        >
+          <img
+            src={KakaoLogo}
+            alt="카카오 아이콘"
+            className={styles.socialIcon}
+          />
+          <span>카카오 계정으로 시작하기</span>
+        </button>
+        <button
+          type="button"
+          className={`${styles.loginBtn} ${styles.googleBtn}`}
+        >
+          <img
+            src={GoogleLogo}
+            alt="구글 아이콘"
+            className={styles.socialIcon}
+          />
+          <span>구글 계정으로 시작하기</span>
+        </button>
+        <button
+          type="button"
+          className={`${styles.loginBtn} ${styles.laterBtn}`}
+          onClick={() => navigate('/timeline')}
+        >
+          <span>나중에 하기</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginModal;
