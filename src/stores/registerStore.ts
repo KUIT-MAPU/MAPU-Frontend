982e9ea6d2678551c31f8803e899a966e33b22d3
ro-el <chung3286@naver.com>
2024-07-11 20:51:26 +0900

    fix(registerStore): 새로고침 및 리렌더링해도 전역 상태 유지되도록 수정
    zustand persist -> local storage 사용

--- src/stores/registerStore.ts
+++ src/stores/registerStore.ts
@@ -1,29 +1,46 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { RegisterStatus } from '../types/RegisterStatus';
 
 interface State {
-  register: RegisterStatus;
+  registerStatus: RegisterStatus;
   accessToken: string;
   refreshToken: string;
-  setStatus: (status: RegisterStatus) => void;
+  setRegisterStatus: (status: RegisterStatus) => void;
   resetStatus: () => void; //logOut으로 사용 가능
   setAccessToken: (token: string) => void;
   setRefreshToken: (refreshToken: string) => void;
 }
 
-const useRegisterStore = create<State>((set) => ({
-  register: RegisterStatus.NEED_LOG_IN,
-  accessToken: '',
-  refreshToken: '',
-  setStatus: (status) => set((state) => ({ ...state, register: status })),
-  resetStatus: () =>
-    set({
-      register: RegisterStatus.NEED_LOG_IN,
+const useRegisterStore = create(
+  persist<State>(
+    (set) => ({
+      registerStatus: RegisterStatus.NEED_LOG_IN,
       accessToken: '',
       refreshToken: '',
+      setRegisterStatus: (status) => {
+        console.log(status + '로 상태 수정');
+        set((state) => ({ ...state, registerStatus: status }));
+      },
+      resetStatus: () => {
+        resetStorage();
+        set({
+          registerStatus: RegisterStatus.NEED_LOG_IN,
+          accessToken: '',
+          refreshToken: '',
+        });
+      },
+      setAccessToken: (token) => {
+        set({ accessToken: token });
+      },
+      setRefreshToken: (token) => {
+        set({ refreshToken: token });
+      },
     }),
-  setAccessToken: (token) => set({ accessToken: token }),
-  setRefreshToken: (token) => set({ refreshToken: token }),
-}));
+    { name: 'registerStatusStorage' },
+  ),
+);
+
+const resetStorage = () => useRegisterStore.persist.clearStorage();
 
 export default useRegisterStore;
