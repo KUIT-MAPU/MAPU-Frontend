1fb462d7c3d29d02b7536875614cfe548d4b8a23
ro-el <chung3286@naver.com>
2024-07-11 22:47:16 +0900

    feat(AuthContainer): 상태 변경 함수 수정

--- src/components/login/AuthContainer.tsx
+++ src/components/login/AuthContainer.tsx
@@ -6,7 +6,7 @@ import { RegisterStatus } from '../../types/RegisterStatus';
 import { useEffect } from 'react';
 
 const AuthContainer = () => {
-  const { registerStatus, setRegisterStatus } = useRegisterStore();
+  const { registerStatus, resetStatus } = useRegisterStore();
   const [searchParams] = useSearchParams();
   const authState = searchParams.get('authState');
 
@@ -15,7 +15,7 @@ const AuthContainer = () => {
       authState !== 'signup' && //쿼리스트링으로 전달된 authState 값이 signup이 아니고
       registerStatus !== RegisterStatus.LOG_IN //이미 로그인 되어있는 상태가 아니면
     ) {
-      setRegisterStatus(RegisterStatus.NEED_LOG_IN); //로그인이 필요한 상태로 재설정
+      resetStatus(); //로그인이 필요한 상태로 재설정
     }
   }, [authState]);
 
