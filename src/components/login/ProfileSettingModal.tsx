7c6ab3299e2ce517cae7b39dca382927c64f3f8e
ro-el <chung3286@naver.com>
2024-07-14 19:48:18 +0900

    refactor: 프로필세팅 모달에서 사용자가 실제로 입력하는 부분 ProfileInfoSetting  컴포넌트로 분리
    프로필 이미지, 닉네임, id 입력 및 시작/적용하기 버튼

--- src/components/login/ProfileSettingModal.tsx
+++ src/components/login/ProfileSettingModal.tsx
@@ -1,13 +1,12 @@
 import styles from './ProfileSettingModal.module.scss';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { RegisterStatus } from '../../types/RegisterStatus';
 
-import UserDefaultImage from '../../assets/user-default-image.svg';
-import ProfileEditPen from '../../assets/profile-edit-pen.svg';
-import InfoGrayCircle from '../../assets/info-gray-circle.svg';
 import ProfileSetting from '../../assets/profile_setting.svg';
 import Close from '../../assets/close.svg';
-import { useLocation, useNavigate } from 'react-router-dom';
+
 import useRegisterStore from '../../stores/registerStore';
-import { RegisterStatus } from '../../types/RegisterStatus';
+import ProfileInfoSetting from './ProfileInfoSetting';
 
 const ProfileSettingModal = () => {
   const { registerStatus } = useRegisterStore();
@@ -21,47 +20,7 @@ const ProfileSettingModal = () => {
   };
 
   if (isWelcome) {
-    return (
-      <div className={styles.welcomeProfileSettingContainer}>
-        <div className={styles.userImgContainer}>
-          <div className={styles.userImg}>
-            <img src={UserDefaultImage} alt="사용자 기본 프로필 이미지" />
-            <button type="button" className={styles.profileEditBtn}>
-              <img src={ProfileEditPen} alt="프로필 이미지 편집 버튼" />
-            </button>
-          </div>
-        </div>
-        <div className={styles.userDataContainer}>
-          <div className={styles.userDataInputContainer}>
-            <div className={styles.inputContainer}>
-              <input type="text" placeholder="닉네임을 입력하세요." />
-              <div className={styles.valueInfoContainer}>
-                <div className={styles.valueInfo}>
-                  <img src={InfoGrayCircle} alt="회색 안내 아이콘" />
-                  <span>3~12글자 이내</span>
-                </div>
-              </div>
-            </div>
-            <div className={styles.inputContainer}>
-              <input type="text" placeholder="아이디를 입력하세요." />
-              <div className={styles.valueInfoContainer}>
-                <div className={styles.valueInfo}>
-                  <img src={InfoGrayCircle} alt="회색 안내 아이콘" />
-                  <span>3~20글자 이내</span>
-                </div>
-                <div className={styles.valueInfo}>
-                  <img src={InfoGrayCircle} alt="회색 안내 아이콘" />
-                  <span>한글 미사용</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button type="submit" className={styles.signUpBtn} disabled>
-            <span>시작하기</span>
-          </button>
-        </div>
-      </div>
-    );
+    return <ProfileInfoSetting />;
   }
 
   return (
@@ -75,54 +34,12 @@ const ProfileSettingModal = () => {
             <span className={styles.info__text}>프로필 편집</span>
           )}
         </div>
-        <button type="button" className={styles.cancelBtn}>
+        <button type="button">
           <img src={Close} alt="취소 버튼" onClick={handelCancel} />
         </button>
       </div>
 
-      <div className={styles.welcomeProfileSettingContainer}>
-        <div className={styles.userImgContainer}>
-          <div className={styles.userImg}>
-            <img src={UserDefaultImage} alt="사용자 기본 프로필 이미지" />
-            <button type="button" className={styles.profileEditBtn}>
-              <img src={ProfileEditPen} alt="프로필 이미지 편집 버튼" />
-            </button>
-          </div>
-        </div>
-        <div className={styles.userDataContainer}>
-          <div className={styles.userDataInputContainer}>
-            <div className={styles.inputContainer}>
-              <input type="text" placeholder="닉네임을 입력하세요." />
-              <div className={styles.valueInfoContainer}>
-                <div className={styles.valueInfo}>
-                  <img src={InfoGrayCircle} alt="회색 안내 아이콘" />
-                  <span>3~12글자 이내</span>
-                </div>
-              </div>
-            </div>
-            <div className={styles.inputContainer}>
-              <input type="text" placeholder="아이디를 입력하세요." />
-              <div className={styles.valueInfoContainer}>
-                <div className={styles.valueInfo}>
-                  <img src={InfoGrayCircle} alt="회색 안내 아이콘" />
-                  <span>3~20글자 이내</span>
-                </div>
-                <div className={styles.valueInfo}>
-                  <img src={InfoGrayCircle} alt="회색 안내 아이콘" />
-                  <span>한글 미사용</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button type="submit" className={styles.signUpBtn} disabled>
-            {registerStatus === RegisterStatus.SIGNING_UP ? (
-              <span>시작하기</span>
-            ) : (
-              <span>적용하기</span>
-            )}
-          </button>
-        </div>
-      </div>
+      <ProfileInfoSetting />
     </div>
   );
 };
