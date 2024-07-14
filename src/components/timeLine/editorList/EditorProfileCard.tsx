676578db5501e96b332bd8b976c088d8e1b47189
leunbin <155026969+leunbin@users.noreply.github.com>
2024-07-14 13:16:33 +0900

    feat: 추천에디터리스트, 키워드 리스트 구현

---
+++ src/components/timeLine/editorList/EditorProfileCard.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import styles from './EditorProfileCard.module.scss';
+import dimmedStyles from '../Dimmed.module.scss';
+import { EditorType } from "../../../types/EditorType";
+import useRegisterStore from "../../../stores/registerStore";
+import { RegisterStatus } from "../../../types/RegisterStatus";
+
+import LoginModal from "../../login/LoginModal";
+import AuthContainer from "../../login/AuthContainer";
+import ProfileSettingModal from "../../login/ProfileSettingModal";
+
+
+interface ProfileCardProps {
+  Editor: EditorType;
+}
+
+const EditorProfileCard: React.FC<ProfileCardProps> = ({ Editor }) => {
+  const registerStore = useRegisterStore();
+  const [dimmed, setDimmed] = useState<boolean>(false);
+  const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+
+  const isLoggedIn = () => {
+    return registerStore.registerStatus === RegisterStatus.LOG_IN;
+  };
+
+  const handleFollow = () => {
+    if(!isLoggedIn()) {
+      setShowLoginModal(true);
+      setDimmed(true);
+    }
+  }
+
+  const handleCloseModal = () => {
+    setShowLoginModal(false);
+    setDimmed(false);
+  }
+
+  return (
+    <div className={styles.cardRoot}>
+      {dimmed && <div className={dimmedStyles.background}></div>}
+      <div className={styles.editorInfo}>
+        <img className={styles.editorImg} src={Editor.img} alt="Editor Image" />
+
+        <div className={styles.editorNameNid}>
+          <div className={styles.editorName}>{Editor.name}</div>
+          <span className={styles.editorId}>@{Editor.userId}</span>
+        </div>
+      </div>
+
+      <button className={styles.following} onClick={handleFollow}>{Editor.following ? "팔로잉" : "팔로우"}</button>
+
+      {/* {showLoginModal && <LoginModal className={styles.LoginModal} laterBtnClick={}/>} */}
+      { showLoginModal 
+        && <AuthContainer className={styles.AuthContainer} />
+      }
+    </div>
+  );
+};
+
+export default EditorProfileCard;
---
+++ src/types/EditorType.ts
@@ -0,0 +1,7 @@
+export interface EditorType {
+  id : number;
+  userId: string;
+  img : string;
+  name : string;
+  following : boolean;
+}
