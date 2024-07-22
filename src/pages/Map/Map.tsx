8d95301b670cd996e2e3f0e033d52f94a18efe9b
ro-el <chung3286@naver.com>
2024-07-22 22:46:49 +0900

    docs: todo 주석 추가

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -8,6 +8,8 @@ const Map = () => {
 
   useEffect(() => {
     const titleElement = document.getElementsByTagName('title')[0];
+    //TODO: 지도 제목이 바뀐 경우, 이전으로 갔을 때 오류 반환해야 함
+    //mapId를 통해 지도 정보 api 호출 -> 지도 제목이 일치하는지 확인 -> 불일치시 에러
     titleElement.innerHTML = `${mapName!.replaceAll('-', ' ')} | MAPU`;
   }, [mapName]);
   return (
