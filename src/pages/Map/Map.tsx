e83b3330e3da4655c34bf0773a9aed2bf2404b19
ro-el <chung3286@naver.com>
2024-07-24 19:30:46 +0900

    feat(Map): 지도 권한 여부에 따른 로직 구현
    publish한 지도의 경우, 권한이 어떻게 설정되는지 확인 후 수정 필요

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,14 +1,35 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import styles from './Map.module.scss';
+import dimmedStyles from '../../components/timeLine/Dimmed.module.scss';
 import MapInfoPanel from '../../components/map/MapInfoPanel/MapInfoPanel';
+import useMapInfoStore from '../../stores/mapInfoStore';
+import useRegisterStore from '../../stores/registerStore';
+import { RegisterStatus } from '../../types/RegisterStatus';
+import AuthContainer from '../../components/login/AuthContainer';
 
 const Map = () => {
+  const navigate = useNavigate();
+  const pathname = useLocation().pathname;
   const { mapName } = useParams();
+  const { isPublished, isMine, amIEditor } = useMapInfoStore();
+  const { registerStatus, setLoginNeeded } = useRegisterStore();
+  const [dimmed, setDimmed] = useState<boolean>(false);
 
   //TODO: 권한 판단
-  //1. 로그인 여부
-  //2. 지도 제작자 여부
-  //3. 편집자 여부
+  //[공유 링크, 일반 링크]
+  //1. 지도의 publish 여부
+  //1.1 pulish -> 지도 뷰어 표시 //확인 후 수정
+  //1.2 publish X -> 1.2.1
+  //1.2.1 로그인 여부
+  //1.2.1.1 로그인 O -> 1.2.2
+  //1.2.1.2 로그인 X -> 로그인 모달
+  //1.2.2 지도 제작자 여부
+  //1.2.2.1 지도 제작자 O -> 지도 에디터 표시
+  //1.2.2.2 지도 제작자 X -> 1.2.3
+  //1.2.3 편집자 여부
+  //1.2.3.1 편집자 O -> 지도 데이터 표시(지도 제작자 프로필, 북마크 포함)
+  //1.2.3.2 편집자 X -> 지도 뷰어 표시
 
   //TODO: 지도 정보 api 호출 -> react query의 캐시로 데이터 관리
 
@@ -19,11 +40,35 @@ const Map = () => {
     titleElement.innerHTML = `${mapName!.replaceAll('-', ' ')} | MAPU`;
   }, [mapName]);
 
-  return (
-    <>
-      <MapInfoPanel />
-    </>
-  );
+  useEffect(() => {
+    if (!isPublished) {
+      if (registerStatus !== RegisterStatus.LOG_IN) {
+        setLoginNeeded(true);
+        setDimmed(true);
+      } else {
+        setDimmed(false);
+      }
+    }
+  }, [isPublished, registerStatus]);
+
+  const handleClose = () => {
+    setLoginNeeded(false);
+    const prevUrl = pathname.split('?')[0];
+    navigate(prevUrl);
+  };
+
+  if (isMine || (!isMine && amIEditor))
+    //지도 에디터
+    return (
+      <>
+        {dimmed && (
+          <div className={dimmedStyles.background} onClick={handleClose} />
+        )}
+        {dimmed && <AuthContainer className={styles.authContainer} />}
+        <MapInfoPanel />
+      </>
+    );
+  return <>지도 뷰어</>;
 };
 
 export default Map;
