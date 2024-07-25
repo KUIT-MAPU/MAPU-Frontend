0af7e7be3789a4b102f8ded25d0ce0198c11f20d
ro-el <chung3286@naver.com>
2024-07-25 23:35:55 +0900

    feat(mapInfoStore): 객체 개요 리스트 전역 상태로 관리, 공유 링크 정보 추가

--- src/components/map/MapInfoPanel/ObjectList.tsx
+++ src/components/map/MapInfoPanel/ObjectList.tsx
@@ -1,76 +1,9 @@
-import { useState } from 'react';
-import { ObjectShape } from '../../../types/enum/ObjectShape';
-import { ObjectOutline } from '../../../types/ObjectOutline';
 import styles from './ObjectList.module.scss';
 import ObjectOutlineBtn from './ObjectOutlineBtn';
 import useMapInfoStore from '../../../stores/mapInfoStore';
 
-const mockObjectList: ObjectOutline[] = [
-  {
-    objectId: 1,
-    shape: ObjectShape.POINT,
-    name: '용용선생',
-  },
-  {
-    objectId: 2,
-    shape: ObjectShape.LINE,
-    name: '식후 건대 호수 한 바퀴',
-  },
-  {
-    objectId: 3,
-    shape: ObjectShape.LINE,
-    name: '건대에서 어대까지',
-  },
-  {
-    objectId: 4,
-    shape: ObjectShape.PLANE,
-    name: '중국집 거리',
-  },
-  {
-    objectId: 11,
-    shape: ObjectShape.POINT,
-    name: '용용선생',
-  },
-  {
-    objectId: 21,
-    shape: ObjectShape.LINE,
-    name: '식후 건대 호수 한 바퀴',
-  },
-  {
-    objectId: 31,
-    shape: ObjectShape.LINE,
-    name: '건대에서 어대까지',
-  },
-  {
-    objectId: 41,
-    shape: ObjectShape.PLANE,
-    name: '중국집 거리',
-  },
-  {
-    objectId: 12,
-    shape: ObjectShape.POINT,
-    name: '용용선생',
-  },
-  {
-    objectId: 22,
-    shape: ObjectShape.LINE,
-    name: '식후 건대 호수 한 바퀴',
-  },
-  {
-    objectId: 32,
-    shape: ObjectShape.LINE,
-    name: '건대에서 어대까지',
-  },
-  {
-    objectId: 42,
-    shape: ObjectShape.PLANE,
-    name: '중국집 거리',
-  },
-];
-
 const ObjectList = () => {
-  const [objectData, setObjectData] = useState<ObjectOutline[]>(mockObjectList);
-  const { isMine } = useMapInfoStore();
+  const { isMine, objectOutlineList } = useMapInfoStore();
 
   return (
     <div className={styles.objectListContainer}>
@@ -84,8 +17,10 @@ const ObjectList = () => {
             : `${styles.objectList} ${styles.notMineList}`
         }
       >
-        {objectData &&
-          objectData.map((object) => <ObjectOutlineBtn object={object} />)}
+        {objectOutlineList &&
+          objectOutlineList.map((object) => (
+            <ObjectOutlineBtn object={object} />
+          ))}
       </div>
     </div>
   );
--- src/stores/mapInfoStore.ts
+++ src/stores/mapInfoStore.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { MapInfo } from '../types/MapInfo';
+import { ObjectOutline } from '../types/ObjectOutline';
+import { ObjectShape } from '../types/enum/ObjectShape';
 
 interface State {
   mapId: number;
@@ -10,8 +12,10 @@ interface State {
   centerLatitude: number;
   centerLongitude: number;
   isPublished: boolean;
+  publicLink?: string;
   isMine: boolean;
   isBookmarked?: boolean;
+  objectOutlineList: ObjectOutline[];
   setMapInfo: (mapInfo: MapInfo) => void;
   setTitle: (title: string) => void;
   setDescription: (description: string) => void;
@@ -29,7 +33,70 @@ const useMapInfoStore = create(
       centerLatitude: 0.0,
       centerLongitude: 0.0,
       isPublished: false,
+      publicLink: 'mapu.com/publicLink',
       isMine: false,
+      objectOutlineList: [
+        {
+          objectId: 1,
+          shape: ObjectShape.POINT,
+          name: '용용선생',
+        },
+        {
+          objectId: 2,
+          shape: ObjectShape.LINE,
+          name: '식후 건대 호수 한 바퀴',
+        },
+        {
+          objectId: 3,
+          shape: ObjectShape.LINE,
+          name: '건대에서 어대까지',
+        },
+        {
+          objectId: 4,
+          shape: ObjectShape.PLANE,
+          name: '중국집 거리',
+        },
+        {
+          objectId: 11,
+          shape: ObjectShape.POINT,
+          name: '용용선생',
+        },
+        {
+          objectId: 21,
+          shape: ObjectShape.LINE,
+          name: '식후 건대 호수 한 바퀴',
+        },
+        {
+          objectId: 31,
+          shape: ObjectShape.LINE,
+          name: '건대에서 어대까지',
+        },
+        {
+          objectId: 41,
+          shape: ObjectShape.PLANE,
+          name: '중국집 거리',
+        },
+        {
+          objectId: 12,
+          shape: ObjectShape.POINT,
+          name: '용용선생',
+        },
+        {
+          objectId: 22,
+          shape: ObjectShape.LINE,
+          name: '식후 건대 호수 한 바퀴',
+        },
+        {
+          objectId: 32,
+          shape: ObjectShape.LINE,
+          name: '건대에서 어대까지',
+        },
+        {
+          objectId: 42,
+          shape: ObjectShape.PLANE,
+          name: '중국집 거리',
+        },
+      ],
       setMapInfo: (mapInfo) =>
         set({
           mapId: mapInfo.id,
@@ -39,6 +106,7 @@ const useMapInfoStore = create(
           centerLatitude: mapInfo.latitude,
           centerLongitude: mapInfo.longitude,
           isPublished: mapInfo.isPublished,
+          publicLink: mapInfo.isPublished ? mapInfo.publicLink : '',
           isMine: mapInfo.isMine,
           isBookmarked: mapInfo.isMine && mapInfo.isBookmarked,
         }),
