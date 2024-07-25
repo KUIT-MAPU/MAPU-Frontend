3fb96346c42ca8f2cf58d0d3b037f78d62769b45
ro-el <chung3286@naver.com>
2024-07-25 23:39:50 +0900

    feat(PublishLinkContainer): 지도 게시 및 공유 링크 복사 컴포넌트 구현
    에디터: 게시/게시취소, 뷰어: 공유 링크 복사 컴포넌트

--- src/components/map/ObjectInfoPanel/ObjectInfoPanel.tsx
+++ src/components/map/ObjectInfoPanel/ObjectInfoPanel.tsx
@@ -1,11 +1,16 @@
 import styles from './ObjectInfoPanel.module.scss';
+import PublishLinkContainer from './PublishLinkContainer';
 
 interface Props {
   mode: string;
 }
 
 const ObjectInfoPanel: React.FC<Props> = ({ mode }) => {
-  return <div id={styles.objectInfoPanel}>ObjectInfoPanel</div>;
+  return (
+    <div id={styles.objectInfoPanel}>
+      <PublishLinkContainer mode={mode} />
+    </div>
+  );
 };
 
 export default ObjectInfoPanel;
