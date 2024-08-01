2d44360253d7dcad2a8dca1304f67cda04068db8
ro-el <chung3286@naver.com>
2024-08-01 16:02:58 +0900

    refactor(ObjectBasicInfo type): 객체 기본 정보 데이터 타입 수정
    에디터, 뷰어 병합

--- src/types/map/object/ObjectBasicInfo.ts
+++ src/types/map/object/ObjectBasicInfo.ts
@@ -1,14 +1,6 @@
 import { ObjectShape } from '../../enum/ObjectShape';
 
-export interface EditorObjectBasicInfo {
-  shape: ObjectShape;
-  roadNameAddress: string;
-  length?: string; //길이 - 선
-  perimeter?: string; //둘레 - 면
-  area?: string; //면적 - 면
-}
-
-export interface ViewerObjectBasicInfo {
+export interface ObjectBasicInfo {
   shape: ObjectShape;
   name: string;
   roadNameAddress: string;
