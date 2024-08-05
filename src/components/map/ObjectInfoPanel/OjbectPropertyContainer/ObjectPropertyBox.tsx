9d2cf6afd25609d2f36317d4d09c222e77105483
ro-el <chung3286@naver.com>
2024-08-05 16:05:44 +0900

    feat(ObjectPropertyBox): 객체 정보 속성의 연결 부분 구현

--- src/components/map/ObjectInfoPanel/OjbectPropertyContainer/ObjectPropertyBox.tsx
+++ src/components/map/ObjectInfoPanel/OjbectPropertyContainer/ObjectPropertyBox.tsx
@@ -1,17 +1,40 @@
 import styles from './ObjectPropertyBox.module.scss';
 import { ObjectOutline } from '../../../../types/map/object/ObjectOutline';
-import { ObjectPropertyType } from '../../../../types/enum/ObjectPropertyType';
 import { StarRating } from '../../../../types/map/object/StarRating';
+import { ObjectPropertyType } from '../../../../types/enum/ObjectPropertyType';
+import { ObjectShape } from '../../../../types/enum/ObjectShape';
 import MenuBtn from '../../../../assets/btn_menu_gray.svg';
+import Point from '../../../../assets/map/ico_point.svg';
+import Line from '../../../../assets/map/ico_line.svg';
+import Plane from '../../../../assets/map/ico_point.svg';
 
 interface Props {
   type: ObjectPropertyType;
   values: ObjectOutline[] | string[] | StarRating[];
 }
 
+// 타입 가드 함수들
+const isObjectOutlineArray = (
+  values: ObjectOutline[] | string[] | StarRating[],
+): values is ObjectOutline[] => {
+  return (values as ObjectOutline[])[0] !== undefined;
+};
+
+const isTagArray = (
+  values: ObjectOutline[] | string[] | StarRating[],
+): values is string[] => {
+  return typeof (values as string[])[0] === 'string';
+};
+
+const isStarRatingArray = (
+  values: ObjectOutline[] | string[] | StarRating[],
+): values is StarRating[] => {
+  return (values as StarRating[])[0] !== undefined;
+};
+
 const ObjectPropertyBox: React.FC<Props> = ({ type, values }) => {
   //connections
-  if (type === ObjectPropertyType.CONNECTION)
+  if (type === ObjectPropertyType.CONNECTION && isObjectOutlineArray(values))
     return (
       <div className={styles.objectPropertyBox}>
         <div className={styles.propertyTitleContainer}>
@@ -20,12 +43,34 @@ const ObjectPropertyBox: React.FC<Props> = ({ type, values }) => {
             <img src={MenuBtn} alt="메뉴" />
           </button>
         </div>
-        <div className={styles.propertyBox}></div>
+        <div className={styles.propertyBox}>
+          {values.map((connectionObject) => (
+            <div className={styles.connectionBox}>
+              <div className={styles.objectShapeBox}>
+                {connectionObject.shape === ObjectShape.POINT ? (
+                  <img src={Point} alt="점 객체" />
+                ) : connectionObject.shape === ObjectShape.LINE ? (
+                  <img src={Line} alt="선 객체" />
+                ) : (
+                  <img src={Plane} alt="면 객체" />
+                )}
+              </div>
+              <div className={styles.objectOutlineBox}>
+                <span className={styles.objectName}>
+                  {connectionObject.name}
+                </span>
+                <span className={styles.objectRoadNameAddress}>
+                  {connectionObject.roadNameAddress}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
 
   //tags
-  if (type === ObjectPropertyType.TAG)
+  if (type === ObjectPropertyType.TAG && isTagArray(values))
     return (
       <div className={styles.objectPropertyBox}>
         <div className={styles.propertyTitleContainer}>
@@ -39,17 +84,20 @@ const ObjectPropertyBox: React.FC<Props> = ({ type, values }) => {
     );
 
   //star ratings
-  return (
-    <div className={styles.objectPropertyBox}>
-      <div className={styles.propertyTitleContainer}>
-        <span className={styles.propertyTitle}>별점</span>
-        <button type="button" className={styles.propertyMenuBtn}>
-          <img src={MenuBtn} alt="메뉴" />
-        </button>
+  if (type === ObjectPropertyType.STAR_RATING && isStarRatingArray(values))
+    return (
+      <div className={styles.objectPropertyBox}>
+        <div className={styles.propertyTitleContainer}>
+          <span className={styles.propertyTitle}>별점</span>
+          <button type="button" className={styles.propertyMenuBtn}>
+            <img src={MenuBtn} alt="메뉴" />
+          </button>
+        </div>
+        <div className={styles.propertyBox}></div>
       </div>
-      <div className={styles.propertyBox}></div>
-    </div>
-  );
+    );
+
+  return <></>; //잘못된 객체 정보 속성 타입
 };
 
 export default ObjectPropertyBox;
