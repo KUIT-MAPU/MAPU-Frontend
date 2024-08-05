beaa3e0bccf5753e98aae6588501fb84ec57b4c2
ro-el <chung3286@naver.com>
2024-08-05 15:27:10 +0900

    feat(ObjectPropertyBox): 객체 정보 속성 컴포넌트 공통 부분 스타일링

--- src/components/map/ObjectInfoPanel/OjbectPropertyContainer/ObjectPropertyBox.tsx
+++ src/components/map/ObjectInfoPanel/OjbectPropertyContainer/ObjectPropertyBox.tsx
@@ -2,6 +2,7 @@ import styles from './ObjectPropertyBox.module.scss';
 import { ObjectOutline } from '../../../../types/map/object/ObjectOutline';
 import { ObjectPropertyType } from '../../../../types/enum/ObjectPropertyType';
 import { StarRating } from '../../../../types/map/object/StarRating';
+import MenuBtn from '../../../../assets/btn_menu_gray.svg';
 
 interface Props {
   type: ObjectPropertyType;
@@ -12,15 +13,43 @@ const ObjectPropertyBox: React.FC<Props> = ({ type, values }) => {
   //connections
   if (type === ObjectPropertyType.CONNECTION)
     return (
-      <div className={styles.objectPropertyBox}>object connections</div>
+      <div className={styles.objectPropertyBox}>
+        <div className={styles.propertyTitleContainer}>
+          <span className={styles.propertyTitle}>객체 연결</span>
+          <button type="button" className={styles.propertyMenuBtn}>
+            <img src={MenuBtn} alt="메뉴" />
+          </button>
+        </div>
+        <div className={styles.propertyBox}></div>
+      </div>
     );
 
   //tags
   if (type === ObjectPropertyType.TAG)
-    return <div className={styles.objectPropertyBox}>tags</div>;
+    return (
+      <div className={styles.objectPropertyBox}>
+        <div className={styles.propertyTitleContainer}>
+          <span className={styles.propertyTitle}>태그</span>
+          <button type="button" className={styles.propertyMenuBtn}>
+            <img src={MenuBtn} alt="메뉴" />
+          </button>
+        </div>
+        <div className={styles.propertyBox}></div>
+      </div>
+    );
 
   //star ratings
-  return <div className={styles.objectPropertyBox}>star rating</div>;
+  return (
+    <div className={styles.objectPropertyBox}>
+      <div className={styles.propertyTitleContainer}>
+        <span className={styles.propertyTitle}>별점</span>
+        <button type="button" className={styles.propertyMenuBtn}>
+          <img src={MenuBtn} alt="메뉴" />
+        </button>
+      </div>
+      <div className={styles.propertyBox}></div>
+    </div>
+  );
 };
 
 export default ObjectPropertyBox;
