758f98ec1854f94469a1eb9b5423585da67ed6bc
Demian <sykc2002@gmail.com>
2024-08-07 06:24:34 +0900

    feat: newMap modal in pregress and GlobalNavigation bar fixed

--- src/components/global/GlobalNavigationBar.tsx
+++ src/components/global/GlobalNavigationBar.tsx
@@ -42,38 +42,6 @@ const GlobalNavigationBar = (props: { children?: React.ReactNode }) => {
       <main className={styles.main}>{props.children}</main>
     </div>
   );
-  return (
-    <div className={styles.container}>
-      <div className={styles.LeftSideBar}>
-        <div className={styles.logoIconContainer}>
-          <MapuLogo className={styles.icon} />
-        </div>
-        <Link to="/" className={styles.link}>
-          <div className={styles.iconContainer}>
-            <Home className={styles.icon} />
-          </div>
-        </Link>
-        <Link to="/explore" className={styles.link}>
-          <div className={styles.iconContainer}>
-            <Explore className={styles.icon} />
-          </div>
-        </Link>
-        <Link to="/user" className={styles.link}>
-          <div
-            className={`${styles.iconContainer} ${styles.userIconContainer}`}
-          >
-            <User className={styles.icon} />
-          </div>
-        </Link>
-        <div
-          className={`${styles.iconContainer} ${styles.bottomIconContainer}`}
-        >
-          <Login className={styles.icon} />
-        </div>
-      </div>
-      {props.children && <main className={styles.main}>{props.children}</main>}
-    </div>
-  );
 };
 
 export default GlobalNavigationBar;
