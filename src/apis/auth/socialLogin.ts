f44b4bed29a37e86850db3003b616a77d261caa6
ro-el <chung3286@naver.com>
2024-08-12 20:46:02 +0900

    feat(socialLogin): 소셜 로그인 api 연동

--- src/apis/auth/socialLogin.ts
+++ src/apis/auth/socialLogin.ts
@@ -1,47 +1,26 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import instance from '../instance';
 import { BaseResponse } from '../../types/BaseResponse';
+import { LoginSuccess } from '../../types/auth/login';
 
-export const kakaoLogin = async (code: string) => {
-  // console.log('kakaoLogin');
-  const response = await instance.get<BaseResponse>(
-    `/user/signin/kakao?code=${code}`,
-  );
-  console.log(response);
-  console.log(response.data);
-  return response.data;
-};
-
-export const googleLogin = async (code: string) => {
-  console.log('googleLogin');
-  const response = await instance.get<BaseResponse>(
-    `/user/signin/google?code=${code}`,
-  );
-  console.log(response);
-  console.log(response.data);
-  return response.data;
-};
-
-export const useKakaoUserDataQuery = (code: string) => {
-  const { data: kakaoUserData, isLoading: isKakaoUserDataLoading } = useQuery({
-    queryKey: ['kakaoLogin'],
-
-    // 쿼리 함수를 설정. 이 함수는 API 호출을 담당하며, 여기서는 fetchFeedsData 함수를 호출
-    queryFn: () => kakaoLogin(code),
-  });
-
-  return { kakaoUserData, isKakaoUserDataLoading };
-};
-
-export const useGoogleUserDataQuery = (code: string) => {
-  const { data: googleUserData, isLoading: isGoogleUserDataLoading } = useQuery(
+export const socialLogin = async (social: string, code: string) => {
+  const response = await instance.post<BaseResponse<LoginSuccess>>(
+    `/user/signin`,
     {
-      queryKey: ['googleLogin'],
-
-      // 쿼리 함수를 설정. 이 함수는 API 호출을 담당하며, 여기서는 fetchFeedsData 함수를 호출
-      queryFn: () => googleLogin(code),
+      socialType: social,
+      code: code,
     },
   );
+  return response.data;
+};
 
-  return { googleUserData, isGoogleUserDataLoading };
+export const useLogInDataQuery = (social: string, code: string) => {
+  const { data: loginData, isLoading: isLoginDataLoading } = useQuery({
+    queryKey: ['socialLogin'],
+
+    // 쿼리 함수를 설정. 이 함수는 API 호출을 담당하며, 여기서는 fetchFeedsData 함수를 호출
+    queryFn: () => socialLogin(social, code),
+  });
+
+  return { loginData, isLoginDataLoading };
 };
