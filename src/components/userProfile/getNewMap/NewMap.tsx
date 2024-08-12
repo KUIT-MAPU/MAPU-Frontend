bc55e59ee874e968bf7e826ee6d6610f72b6bdbd
jyc0328 <sykc2002@gmail.com>
2024-08-13 08:31:47 +0900

    feat: 지도 위치 hover 구현

--- src/components/userProfile/getNewMap/NewMap.tsx
+++ src/components/userProfile/getNewMap/NewMap.tsx
@@ -1,8 +1,19 @@
+import React, {useState} from 'react';
+
 import styles from './NewMap.module.scss';
 import { ReactComponent as ModalClose } from '../../../assets/btn_followmodal_close.svg';
 import { ReactComponent as NewMapIcon } from '../../../assets/ico_newmap.svg';
 
 const NewMap = ({ onClose }: { onClose: () => void }) => {
+
+  const [location,setLocation] = useState('현재 위치');
+
+  const getButtonStyle = (buttonLocation : string) => {
+    return location===buttonLocation
+    ? `${styles.selectedButton}`
+    : `${styles.defaultButton}`;
+  }
+
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modalContent}>
@@ -20,9 +31,15 @@ const NewMap = ({ onClose }: { onClose: () => void }) => {
         </div>
         <div className={styles.mapStartText}>시작 위치</div>
         <div className={styles.btnLocation}>
-          <div>현재 위치</div>
-          <div>전국</div>
-          <div>광역자치단체</div>
+          <button 
+          className={getButtonStyle('현재 위치')} 
+          onClick={() => setLocation('현재 위치')}>현재 위치</button>
+          <button 
+          className={getButtonStyle('전국')}
+          onClick={() => setLocation('전국')}>전국</button>
+          <button 
+          className={getButtonStyle('광역자치단체')}
+          onClick={() => setLocation('광역자치단체')}>광역자치단체</button>
         </div>
         <div className={styles.createBtn}>
           <div>생성하기</div>
