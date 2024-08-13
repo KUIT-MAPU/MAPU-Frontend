3480fa865c915a76f5eff82e54fecaf65ca13759
jyc0328 <sykc2002@gmail.com>
2024-08-13 10:44:48 +0900

    feat: 현재 위치 위도,경도 받아오기 구현

--- src/components/userProfile/getNewMap/NewMap.tsx
+++ src/components/userProfile/getNewMap/NewMap.tsx
@@ -4,11 +4,15 @@ import styles from './NewMap.module.scss';
 import { ReactComponent as ModalClose } from '../../../assets/btn_followmodal_close.svg';
 import { ReactComponent as NewMapIcon } from '../../../assets/ico_newmap.svg';
 
+const {kakao} = window;
+
 const NewMap = ({ onClose }: { onClose: () => void }) => {
 
   const [location,setLocation] = useState<string>('');
   const [mapTitle,setMapTitle] = useState<string>('');
   const [mapCreate,setMapCreate] = useState<boolean>(false);
+  const [latitude,setLatitude] = useState<number | null>(null); //위도(기본값 null)
+  const [longtitude,setLongtitude] = useState<number | null>(null); //경도(기본값 null)
 
   const getButtonStyle = (buttonLocation : string) => {
     return location===buttonLocation
@@ -30,6 +34,23 @@ const NewMap = ({ onClose }: { onClose: () => void }) => {
 
   const getMapCreateStyle = () => {
     return mapCreate ? `${styles.onMapCreate}` : `${styles.offMapCreate}`;
+  }
+  
+  const handleCurrentLocation = () => {
+    setLocation('현재 위치');
+    kakao.maps.load(() => {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const lat=position.coords.latitude;
+          const lon=position.coords.longitude;
+          setLatitude(lat);
+          setLongtitude(lon);
+          console.log(lat,lon);
+        });
+      } else{
+        alert('현재 위치 사용 불가');
+      }
+    })
   }
 
   return (
@@ -55,7 +76,7 @@ const NewMap = ({ onClose }: { onClose: () => void }) => {
         <div className={styles.btnLocation}>
           <button 
           className={getButtonStyle('현재 위치')} 
-          onClick={() => setLocation('현재 위치')}>현재 위치</button>
+          onClick={handleCurrentLocation}>현재 위치</button>
           <button 
           className={getButtonStyle('전국')}
           onClick={() => setLocation('전국')}>전국</button>
