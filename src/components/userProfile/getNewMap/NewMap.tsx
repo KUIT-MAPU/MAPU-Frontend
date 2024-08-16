61b6bf61ac0f1d84326c47570e9072a70405b206
Demian <sykc2002@gmail.com>
2024-08-16 19:25:12 +0900

    feat:MapCreate 백엔드 연동

--- src/components/userProfile/getNewMap/NewMap.tsx
+++ src/components/userProfile/getNewMap/NewMap.tsx
@@ -4,20 +4,21 @@ import styles from './NewMap.module.scss';
 import { ReactComponent as ModalClose } from '../../../assets/btn_followmodal_close.svg';
 import { ReactComponent as NewMapIcon } from '../../../assets/ico_newmap.svg';
 import axios from 'axios';
+import instance from '../../../apis/instance';
 
 const { kakao } = window;
 
 const NewMap = ({ onClose }: { onClose: () => void }) => {
   const [location, setLocation] = useState<string>('');
   const [mapTitle, setMapTitle] = useState<string>('');
-  const [mapDescription, setMapDescription] = useState<string>('');
-  const [imageUrl, setImageUrl] =useState<string>('');
+  const [mapDescription, setMapDescription] = useState<string>('ddd');
+  const [imageUrl, setImageUrl] =useState<string>('ddd');
   const [mapCreate, setMapCreate] = useState<boolean>(false);
   const [latitude, setLatitude] = useState<number | null>(null); //위도(기본값 null)
-  const [longtitude, setLongtitude] = useState<number | null>(null); //경도(기본값 null)
+  const [longitude, setLongitude] = useState<number | null>(null); //경도(기본값 null)
   const [zoomLevel, setZoomLever] = useState<number>(0);
   const [isOnSearch, setIsOnSearch] = useState<boolean>(true);
-  const [keywords,setKeywords] = useState<string[]>([]);
+  const [keywords,setKeywords] = useState<string[]>(["추천"]);
 
   const getButtonStyle = (buttonLocation: string) => {
     return location === buttonLocation
@@ -49,7 +50,7 @@ const NewMap = ({ onClose }: { onClose: () => void }) => {
           const lat = position.coords.latitude;
           const lon = position.coords.longitude;
           setLatitude(lat);
-          setLongtitude(lon);
+          setLongitude(lon);
           console.log(lat, lon);
         });
       } else {
@@ -60,18 +61,18 @@ const NewMap = ({ onClose }: { onClose: () => void }) => {
 
   const sendDataToBackend = async (data:any) => {
     try {
-      const response = await axios.post('/map/create', data);
+      const response = await instance.post('/map/create', data);
       console.log('Data sent successfully:', response.data);
     } catch (error) {
       console.error('Failed to send data:', error);
     }
   };
   
-  // 예를 들어, form 데이터를 백엔드로 전송
+
   const handleSubmit = (event:any) => {
     event.preventDefault();
     
-    if(!mapTitle || !latitude || !longtitude){
+    if(!mapTitle || !latitude || !longitude){
       alert('모든 필수 정보를 입력해주세요');
       return;
     }
@@ -80,13 +81,15 @@ const NewMap = ({ onClose }: { onClose: () => void }) => {
       mapDescription,
       address: location,
       latitude,
-      longtitude,
+      longitude,
       zoomLevel,
-      publishLink : imageUrl, // 예시에서는 imageUrl로 대체
+      publishLink : imageUrl, 
       isOnSearch,
       keywords,
     };
-  
+    
+    console.log('데이터 생성 완료')
+    console.log(formData);
     sendDataToBackend(formData);
   };
   
